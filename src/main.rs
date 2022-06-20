6709179951f138efbf67b3d106bf6945e2952a15
kumac13 <foohilack@gmail.com>
2022-06-21 08:19:55 +0900

    feat:

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+mod cli;
+mod config;
+mod file;
+mod memo;
+use clap::Parser;
+use cli::AppArg;
+use config::Config;
+use file::FileManagement;
+use memo::*;
+use std::path::PathBuf;
+
+fn main() -> anyhow::Result<()> {
+    // parse arg
+    let arg: AppArg = AppArg::parse();
+    let file_name_option = arg.name;
+
+    let config = Config::new();
+
+    // create config
+    if !config.exists() {
+        config.set_config_file()?;
+    }
+
+    // read config
+    let config_path = PathBuf::from(config.set_config_path());
+
+    let memo: Memo = Memo::new(config_path, file_name_option);
+
+    memo.open()?;
+
+    Ok(())
+}
