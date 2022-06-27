186d395c339b3e7222ecf0f5334b1155bce8479c
kumac13 <foohilack@gmail.com>
2022-06-27 09:47:55 +0900

    feat: remove subcommand

--- src/cli.rs
+++ src/cli.rs
@@ -1,16 +1,8 @@
-use clap::{Parser, Subcommand};
+use clap::Parser;
 
 #[derive(Parser, Debug)]
 pub struct AppArg {
     /// Optional File name
     #[clap(short, long, value_parser)]
     pub name: Option<String>,
-
-    #[clap(subcommand)]
-    action: Action,
-}
-
-#[derive(Subcommand, Debug)]
-pub enum Action {
-    DATE,
 }
